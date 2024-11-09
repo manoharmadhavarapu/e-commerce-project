bee78819a2007c7eb49fde20ae1075b62a08f802
manoharmadhavarapu <manohar37nec@gmail.com>
2024-11-09 19:42:43 +0530

    bug fix

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // utils
 const connectDB = require('./config/db');
@@ -19,6 +20,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
